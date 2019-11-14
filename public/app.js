b35d8920d11e2f5673d50b0d696193dfa5a83fd0
chad luo <i@yuki.rocks>
2019-11-14 22:49:06 +1100

    backup

--- public/app.js
+++ public/app.js
@@ -13,93 +13,114 @@ const KEY_DEFAULT_PROJECT = 'defaultProject';
 const KEY_GTD = 'recentGtd';
 const KEY_JIRA_SEARCH = 'recentUrls';
 const RECENT_JIRA_LIMIT = 4;
+const KEY_TEMP_LINKS = 'guerrilla';
 
 const KEY_PREVIOUS_TIP = 'previousTip';
 const KEY_CURRENT_TIP = 'currentTip';
 
-const DEFAULT_PAGE = 'main';
-
-let hasRenderedWinPage = false;
-let hasRenderedMacPage = false;
+const ENDPOINT_GTD = '/gtd';
+const ENDPOINT_JIRA_SEARCH = '/jira';
 
 //#region init
 
-window.history.replaceState(null, null, `${window.location.pathname}#${DEFAULT_PAGE}`);
-if (localStorage.getItem('darkMode')) document.body.classList.add('dark');
+document.addEventListener('DOMContentLoaded', async () => {
+    refreshGtd(); // from localStorage
+    renderJira(); // from localStorage
+    renderGuerrilla(); // from localStorage
+    document.getElementById('defaultProject').value = localStorage.getItem(KEY_DEFAULT_PROJECT) || MY_DEFAULT_PROJECT;
 
-document.addEventListener('DOMContentLoaded', () => {
-  refreshGtd(); // load local
-  renderJira(); // load local
-  document.getElementById('defaultProject').value = localStorage.getItem(KEY_DEFAULT_PROJECT) || MY_DEFAULT_PROJECT;
-  const tipIndex = Math.floor(Math.random() * 304);
-  localStorage.setItem(KEY_PREVIOUS_TIP, localStorage.getItem(KEY_CURRENT_TIP) || '0');
-  localStorage.setItem(KEY_CURRENT_TIP, tipIndex.toString());
-  fetch(`/all?prev=${localStorage.getItem(KEY_PREVIOUS_TIP)}&curr=${localStorage.getItem(KEY_CURRENT_TIP)}`)
-    .then(resolveFetch)
-    .then(json => {
-      store(KEY_GTD, json['gtd']);
-      refreshGtd(); // async load remote
-      store(KEY_JIRA_SEARCH, json['jira']);
-      renderJira(); // async load remote
-      showCurrShortcutTip(json['prev'], json['curr']);
-    })
-    .catch(console.error);
+    const tipIndex = Math.floor(Math.random() * 304);
+    localStorage.setItem(KEY_PREVIOUS_TIP, localStorage.getItem(KEY_CURRENT_TIP) || '0');
+    localStorage.setItem(KEY_CURRENT_TIP, tipIndex.toString());
+
+    const res = await fetch(`/all?prev=${localStorage.getItem(KEY_PREVIOUS_TIP)}&curr=${localStorage.getItem(KEY_CURRENT_TIP)}`);
+    const json = await res.json();
+    showCurrShortcutTip(json['prev'], json['curr']);
+    refreshGtd(json['gtd']); // from backend
+    renderJira(json['jira']); // from backend
+    renderGuerrilla(json['guerrilla']); // from backend
+
+    store(KEY_GTD, json['gtd']);
+    store(KEY_JIRA_SEARCH, json['jira']);
+    store(KEY_TEMP_LINKS, json['guerrilla']);
+
+    setTimeout(() => {
+        const performanceEntry = window.performance.getEntriesByType('navigation')[0];
+        document.getElementById('loadtime').innerText = performanceEntry.duration.toFixed(2) + 'ms';
+    });
 });
 
 //#endregion
 
-//#region common & misc
+//#region common
 
 function load(lsk) {
-  return JSON.parse(localStorage.getItem(lsk)) || [];
+    return JSON.parse(localStorage.getItem(lsk)) || [];
 }
 
 function store(lsk, obj) {
-  localStorage.setItem(lsk, JSON.stringify(obj));
+    localStorage.setItem(lsk, JSON.stringify(obj));
 }
 
 function storeRemote(lsk, endpoint) {
-  const data = localStorage.getItem(lsk);
-  fetch(endpoint, {
-    method: 'PUT',
-    headers: { 'Content-Type': 'application/json;charset=UTF-8' },
-    body: data
-  })
-    .then(console.log)
-    .catch(console.log);
+    const data = localStorage.getItem(lsk);
+    fetch(endpoint, {
+        method: 'PUT',
+        headers: {'Content-Type': 'application/json;charset=UTF-8'},
+        body: data
+    }).then(console.log).catch(console.log);
 }
 
-function resolveFetch(res) {
-  return res.ok ? res.json() : Promise.reject(res);
+function create(endpoint, datum) {
+    fetch(endpoint, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json;charset=UTF-8'},
+        body: JSON.stringify(datum)
+    }).then(console.log).catch(console.error);
+}
+
+async function update(endpoint, data) {
+    const response = await fetch(endpoint, {
+        method: 'PUT',
+        headers: {'Content-Type': 'application/json;charset=UTF-8'},
+        body: JSON.stringify(data)
+    });
+    return await response.json();
 }
 
 document.getElementById('toggleLightDark').addEventListener('click', () => {
-  document.body.classList.add('animated');
-  if (document.body.classList.toggle('dark')) {
-    localStorage.setItem(KEY_DARK_MODE, 't');
-  } else {
-    localStorage.removeItem(KEY_DARK_MODE);
-  }
+    document.body.classList.add('animated');
+    if (document.body.classList.toggle('dark')) {
+        localStorage.setItem(KEY_DARK_MODE, 't');
+    } else {
+        localStorage.removeItem(KEY_DARK_MODE);
+    }
 });
 
-document.getElementById('toCopy').addEventListener('click', event => {
-  const a = event.target;
-  const path = a.getAttribute('data');
-  navigator.clipboard.writeText(path).then(
-    () => {
-      a.innerHTML += ' copied!';
-      console.log('copied path: ' + path);
-    },
-    () => {
-      alert("can't copy");
-    }
-  );
+document.getElementById('toCopy').addEventListener('click', async event => {
+    const a = event.target;
+    if (a.tagName !== 'A') return;
+    await navigator.clipboard.writeText(a.getAttribute('data'));
+    a.innerHTML += ' copied!';
 });
 
 document.getElementById('searchesMinor').addEventListener('submit', event => {
-  const form = event.target;
-  const baseUrl = form.getAttribute('baseurl');
-  window.open(baseUrl + new FormData(form).get('q').trim()).focus();
+    const form = event.target;
+    if (form.tagName !== 'FORM') return;
+    const formData = new FormData(form);
+    window.open(formData.get('url').trim() + formData.get('q').trim()).focus();
+});
+
+document.getElementById('guerrilla').addEventListener('submit', event => {
+    const form = event.target;
+    const formData = new FormData(form);
+    const links = load(KEY_TEMP_LINKS);
+    links.unshift({
+        text: formData.get('g-text'),
+        link: formData.get('g-link')
+    });
+    store(KEY_TEMP_LINKS, links);
+    storeRemote(KEY_TEMP_LINKS, '/guerrilla');
 });
 
 //#endregion
@@ -107,178 +128,178 @@ document.getElementById('searchesMinor').addEventListener('submit', event => {
 //#region date formatter
 
 function formatTimestamp(timestamp, todayBegin) {
-  return formatDate(new Date(Number(timestamp)), todayBegin);
+    return formatDate(new Date(Number(timestamp)), todayBegin);
 }
 
 function formatDate(date, todayBegin) {
-  const timeString = date.toLocaleTimeString();
+    const timeString = date.toLocaleTimeString();
 
-  if (date > todayBegin) return timeString;
+    if (date > todayBegin) return timeString;
 
-  const yesterdayBegin = todayBegin - DAY_MILLIS;
-  if (date > yesterdayBegin) return '昨 ' + timeString;
+    const yesterdayBegin = todayBegin - DAY_MILLIS;
+    if (date > yesterdayBegin) return '昨 ' + timeString;
 
-  const lastSunday = new Date(todayBegin - DAY_MILLIS * todayBegin.getDay());
-  const weekday = WEEKDAYS[date.getDay()];
-  if (date > lastSunday) {
-    return weekday + ' ' + timeString;
-  } else if (date > lastSunday - 7 * DAY_MILLIS) {
-    return '先' + weekday + ' ' + timeString;
-  } else {
-    return date.toLocaleString();
-  }
+    const lastSunday = new Date(todayBegin - DAY_MILLIS * todayBegin.getDay());
+    const weekday = WEEKDAYS[date.getDay()];
+    if (date > lastSunday) {
+        return weekday + ' ' + timeString;
+    } else if (date > lastSunday - 7 * DAY_MILLIS) {
+        return '先' + weekday + ' ' + timeString;
+    } else {
+        return date.toLocaleString();
+    }
 }
 
 //#endregion
 
 //#region jira
 
-function renderJira() {
-  function renderItem(item) {
-    return `<li><a href="${item.url}" title="last search: ${item.access || ''}">${item.fullJiraId}</a> ${item.notes || ''}</li>`;
-  }
+function renderJira(data) {
+    function renderItem(item) {
+        return `<li><a href="${item.url}" title="last search: ${item.access || ''}">${item.fullJiraId}</a> ${item.notes || ''}</li>`;
+    }
 
-  document.getElementById('recentSearches').innerHTML = load(KEY_JIRA_SEARCH)
-    .map(renderItem)
-    .join('');
+    document.getElementById('recentSearches').innerHTML = (data || load(KEY_JIRA_SEARCH)).map(renderItem).join('');
 }
 
-document.getElementById('mainJira').addEventListener('submit', () => {
-  localStorage.setItem(KEY_DEFAULT_PROJECT, document.getElementById('defaultProject').value.trim() || MY_DEFAULT_PROJECT);
-  const jiraId = document.getElementById('jiraId').value.trim();
-  const groups = /(\w+-)?(\d+)/.exec(jiraId);
-  if (!groups || !groups[2]) {
-    window.open(`${JIRA_BASE_URL}/issues/?jql=text ~ "${jiraId}" ORDER BY updated DESC`).focus();
-    return;
-  }
-  const fullJiraId = `${groups[1] || localStorage.getItem(KEY_DEFAULT_PROJECT) + '-'}${groups[2]}`;
-  const url = `${JIRA_BASE_URL}/browse/${fullJiraId}`;
-  window.open(url).focus();
-  const newItem = {
-    fullJiraId: fullJiraId,
-    url: url,
-    access: new Date().toLocaleString(),
-    notes: ''
-  };
-  const items = load(KEY_JIRA_SEARCH);
-  const existingItem = items.findIndex(item => item.fullJiraId === fullJiraId);
-  if (existingItem >= 0) {
-    newItem.notes = items[existingItem].notes;
-    items.splice(existingItem, 1);
-  }
-  items.unshift(newItem);
-  items.splice(RECENT_JIRA_LIMIT);
-  store(KEY_JIRA_SEARCH, items);
-  storeRemote(KEY_JIRA_SEARCH, '/jira');
+document.getElementById('jiraSearch').addEventListener('submit', () => {
+    localStorage.setItem(KEY_DEFAULT_PROJECT, document.getElementById('defaultProject').value.trim() || MY_DEFAULT_PROJECT);
+    const jiraId = document.getElementById('jiraId').value.trim();
+    const groups = /(\w+-)?(\d+)/.exec(jiraId);
+    if (!groups || !groups[2]) {
+        window.open(`${JIRA_BASE_URL}/issues/?jql=text ~ "${jiraId}" ORDER BY updated DESC`).focus();
+        return;
+    }
+    const fullJiraId = `${groups[1] || localStorage.getItem(KEY_DEFAULT_PROJECT) + '-'}${groups[2]}`;
+    const url = `${JIRA_BASE_URL}/browse/${fullJiraId}`;
+    window.open(url).focus();
+    const newItem = {
+        fullJiraId: fullJiraId,
+        url: url,
+        access: new Date().toLocaleString(),
+        notes: ''
+    };
+    const items = load(KEY_JIRA_SEARCH);
+    const existingItem = items.findIndex(item => item.fullJiraId === fullJiraId);
+    if (existingItem >= 0) {
+        newItem.notes = items.splice(existingItem, 1)[0].notes;
+    }
+    items.unshift(newItem);
+    items.splice(RECENT_JIRA_LIMIT);
+    store(KEY_JIRA_SEARCH, items);
+    storeRemote(KEY_JIRA_SEARCH, ENDPOINT_JIRA_SEARCH);
 });
 
 //#endregion
 
 //#region GTD
 
-function refreshGtd() {
-  const tasks = load(KEY_GTD);
-  if (tasks.length === 0) {
-    document.getElementById('gtdItems-active').innerHTML = '';
-    document.getElementById('gtdItems-inactive').innerHTML = '';
-    document.getElementById('emptyTodo').classList.remove('hidden');
-    return;
-  }
-  const today = new Date();
-  const todayBegin = new Date(today.getFullYear(), today.getMonth(), today.getDate());
-  const render = todayBegin => item =>
-    `<li id="${item.id}" class="gtdItem${item.active ? '' : ' inactive'}">
-                  <span onclick="toggleGtdItem(this)">${item.task} | ${formatTimestamp(item.id, todayBegin)}</span></li>`;
-  const renderGtdItem = render(todayBegin);
-  document.getElementById('gtdItems-active').innerHTML = tasks
-    .filter(task => task.active)
-    .map(renderGtdItem)
-    .join('');
-  document.getElementById('gtdItems-inactive').innerHTML = tasks
-    .filter(task => !task.active)
-    .map(renderGtdItem)
-    .join('');
+function refreshGtd(data) {
+    const tasks = data || load(KEY_GTD);
+    if (tasks.length === 0) {
+        document.getElementById('gtdItems-todo').innerHTML = '';
+        document.getElementById('gtdItems-done').innerHTML = '';
+        document.getElementById('emptyTodo').classList.remove('hidden');
+        return;
+    }
+    const today = new Date();
+    const todayBegin = new Date(today.getFullYear(), today.getMonth(), today.getDate());
+    const render = todayBegin => item =>
+        `<li id="${item.id}" class="gtdItem${item.todo ? '' : ' inactive'}">${item.task} <time class="secondary">${formatTimestamp(item.id, todayBegin)}</time></li>`;
+    const renderGtdItem = render(todayBegin);
+    document.getElementById('gtdItems-todo').innerHTML = tasks
+        .filter(task => task.todo)
+        .map(renderGtdItem)
+        .join('');
+    document.getElementById('gtdItems-done').innerHTML = tasks
+        .filter(task => !task.todo)
+        .map(renderGtdItem)
+        .join('');
+    document.getElementById('emptyTodo').classList.add('hidden');
 }
 
-// todo bubble
-function toggleGtdItem(span) {
-  const gtdItem = span.parentElement;
-  gtdItem.classList.toggle('inactive');
-  const tasks = load(KEY_GTD);
-  const itemId = gtdItem.id;
-  const i = tasks.findIndex(task => task.id === itemId);
-  tasks[i].active = !tasks[i].active;
-  store(KEY_GTD, tasks);
-  storeRemote(KEY_GTD, '/gtd');
-  navigator.clipboard.writeText(tasks[i].task).then(
-    () => { },
-    () => {
-      alert("can't copy");
+document.getElementById('gtdItems').addEventListener('click', async event => {
+    const target = event.target;
+    const gtdItem = target.tagName === 'LI' ? target : target.tagName === 'TIME' ? target.parentNode : null;
+    if (!gtdItem) return;
+    gtdItem.classList.toggle('inactive');
+    const tasks = load(KEY_GTD);
+    const i = tasks.findIndex(task => task.id === gtdItem.id);
+    tasks[i].todo = !tasks[i].todo;
+    const res = await update(ENDPOINT_GTD, [tasks[i]]);
+    store(KEY_GTD, res);
+    navigator.clipboard.writeText(tasks[i].task).catch(alert);
+});
+
+document.getElementById('gtdRow').addEventListener('submit', async () => {
+    const taskText = document.getElementById('gtdInbox').value.trim();
+    const groups = /^(\d+) (.+)/.exec(taskText);
+    if (groups && groups[1] && groups[2]) {
+        const items = load(KEY_JIRA_SEARCH);
+        const existingItem = items.findIndex(item => item.fullJiraId.includes(groups[1]));
+        /* attach to jira notes instead of GTD inbox */
+        if (existingItem >= 0) {
+            const item = items.splice(existingItem, 1)[0];
+            item.notes = groups[2];
+            items.unshift(item);
+            store(KEY_JIRA_SEARCH, items);
+            storeRemote(KEY_JIRA_SEARCH, ENDPOINT_JIRA_SEARCH);
+            return;
+        }
     }
-  );
+    create(ENDPOINT_GTD, {
+        id: new Date().getTime().toString(),
+        task: taskText,
+        todo: true,
+        active: true
+    });
+});
+
+document.getElementById('cleanInactiveGtdItems').addEventListener('click', async () => {
+    const data = load(KEY_GTD).filter(e => !e.todo).map(e => {
+        e.active = false;
+        return e;
+    });
+    const gtd = await update(ENDPOINT_GTD, data)
+    store(KEY_GTD, gtd);
+    refreshGtd()
+});
+
+//#endregion
+
+//#region links
+
+function renderGuerrilla(data) {
+    const links = data || load(KEY_TEMP_LINKS);
+    document.getElementById('guerrillaGroup').innerHTML = links.map(link => `<a href="${link.link}">${link.text}</a>`).join(' | ');
 }
-
-document.getElementById('gtdRow').addEventListener('submit', () => {
-  const tasks = load(KEY_GTD);
-  const taskText = document.getElementById('gtdInbox').value.trim();
-  const groups = /^(\d+) ([\w\d\s:]+)/.exec(taskText);
-  if (groups && groups[1] && groups[2]) {
-    const items = load(KEY_JIRA_SEARCH);
-    const existingItem = items.findIndex(item => item.fullJiraId.includes(groups[1]));
-    /* attach to jira notes instead of GTD inbox */
-    if (existingItem >= 0) {
-      items[existingItem].notes = groups[2];
-      store(KEY_JIRA_SEARCH, items);
-      storeRemote(KEY_JIRA_SEARCH, '/jira');
-      return;
-    }
-  }
-  tasks.unshift({
-    id: new Date().getTime().toString(),
-    task: taskText,
-    active: true
-  });
-  store(KEY_GTD, tasks);
-  storeRemote(KEY_GTD, '/gtd');
-});
-
-document.getElementById('cleanInactiveGtdItems').addEventListener('click', () => {
-  store(KEY_GTD, load(KEY_GTD).filter(e => e.active));
-  refreshGtd(); // clean
-  storeRemote(KEY_GTD, '/gtd');
-});
 
 //#endregion
 
 //#region shortcuts
 
-document.getElementById('showShortcutsWin').addEventListener('click', () => {
-  if (hasRenderedWinPage) return;
-  fetch('/shortcuts/win')
-    .then(resolveFetch)
-    .then(shortcuts => renderShortcutsPage(shortcuts, 'shortcutsWin'));
-  hasRenderedWinPage = true;
+document.getElementById('showShortcutsWin').addEventListener('click', async () => {
+    await renderPage('shortcutsWin', '/shortcuts/win');
 });
 
-document.getElementById('showShortcutsMac').addEventListener('click', () => {
-  if (hasRenderedMacPage) return;
-  fetch('/shortcuts/mac')
-    .then(resolveFetch)
-    .then(shortcuts => renderShortcutsPage(shortcuts, 'shortcutsMac'));
-  hasRenderedMacPage = true;
+document.getElementById('showShortcutsMac').addEventListener('click', async () => {
+    await renderPage('shortcutsMac', '/shortcuts/mac');
 });
 
-function renderShortcutsPage(shortcuts, elementId) {
-  console.log(shortcuts);
-  document.getElementById(elementId).innerHTML = `<ul>${shortcuts.map(text => `<li>${text}</li>`).join('')}</ul>`;
+async function renderPage(pageElementId, shortcutsEndpoint) {
+    const shortcutsMacPage = document.getElementById(pageElementId);
+    if (shortcutsMacPage.childNodes.length !== 0) return;
+    const res = await fetch(shortcutsEndpoint);
+    const shortcuts = await res.json();
+    shortcutsMacPage.innerHTML = '<ul>' + shortcuts.map(text => `<li>${text}</li>`).join('') + '</ul>';
 }
 
 function showPrevShortcutTip(prev, curr) {
-  document.getElementById('shortcutTip').innerHTML = `${prev} <a onclick='showCurrShortcutTip("${prev}", "${curr}")'>&#x21b3;</a>`;
+    document.getElementById('shortcutTip').innerHTML = `${prev} <a onclick='showCurrShortcutTip("${prev}", "${curr}")'>&#x21b3;</a>`;
 }
 
 function showCurrShortcutTip(prev, curr) {
-  document.getElementById('shortcutTip').innerHTML = `${curr} <a onclick='showPrevShortcutTip("${prev}", "${curr}")'>&#x21b0;</a>`;
+    document.getElementById('shortcutTip').innerHTML = `${curr} <a onclick='showPrevShortcutTip("${prev}", "${curr}")'>&#x21b0;</a>`;
 }
 
 //#endregion
--- server.js
+++ server.js
@@ -3,77 +3,112 @@
 
 const PORT = 7086;
 
+const SHORTCUTS_WIN = 'assets/shortcuts-win.json';
+const SHORTCUTS_MAC = 'assets/shortcuts-mac.json';
+const DATA_GTD = 'data/gtd.json';
+const DATA_JIRA = 'data/jira.json';
+const DATA_GUERRILLA = 'data/guerrilla.json';
+
 const path = require('path');
 const fsp = require('fs').promises;
-const logger = require('pino')({
-  prettyPrint: {
-    colorize: true,
-    translateTime: true
-  }
-});
+const morgan = require('morgan');
 const express = require('express');
 
 const server = express();
 
 server.use('/public', express.static('public'));
 server.use(express.json());
+server.use(morgan('common'));
+
+// const low = require('lowdb');
+// const FileSync = require('lowdb/adapters/FileSync');
 
 // region Routes
 
 server.get('/', (_req, res) => res.sendFile(path.join(__dirname, 'homepage.html')));
 
-server.put('/gtd', async (req, res) => {
-  logger.info('storing gtd');
-  await fsp.writeFile('data/gtd.json', JSON.stringify(req.body, null, 2), 'utf8');
-  res.send('ok');
+server.all('/gtd', async (req, res) => {
+  const payload = req.body;
+  const data = JSON.parse(await fsp.readFile(DATA_GTD, 'utf8'));
+  switch (req.method) {
+    case 'GET':
+      res.send(data);
+      break;
+    case 'POST': // create
+      data.unshift(payload);
+      await fsp.writeFile(DATA_GTD, JSON.stringify(data, null, 2), 'utf8');
+      res.status(201).send();
+      break;
+    case 'PUT': // update
+      for (let i = 0; i < data.length; i++) {
+        for (let j = 0; j < payload.length; j++) {
+          if (data[i].id === payload[j].id) {
+            data[i] = payload[j];
+          }
+        }
+      }
+      res.send(data.filter(item => item.active));
+      await fsp.writeFile(DATA_GTD, JSON.stringify(data, null, 2), 'utf8');
+      break;
+    default:
+      res.status(405).send();
+      break;
+  }
 });
 
 // JIRA
 
 server.put('/jira', async (req, res) => {
-  logger.info('storing jira');
-  await fsp.writeFile('data/jira.json', JSON.stringify(req.body, null, 2), 'utf8');
+  const payload = req.body;
+  console.log(`storing jira: ${payload.length} items`);
+  await fsp.writeFile('data/jira.json', JSON.stringify(payload, null, 2), 'utf8');
+  res.send('ok');
+});
+
+// dynamic links ('guerrilla')
+
+server.put('/guerrilla', async (req, res) => {
+  const payload = req.body;
+  console.log(`storing guerrilla: ${payload.length} items`);
+  await fsp.writeFile('data/guerrilla.json', JSON.stringify(payload, null, 2), 'utf8');
   res.send('ok');
 });
 
 // yibasuo
 
 server.get('/all', async (req, res) => {
-  function getTip(winTips, macTips, index) {
-    return index < winTips.length ? `[Win] ${winTips[index]}` : `[Mac] ${macTips[index - winTips.length]}`;
-  }
-
   const prev = req.query['prev'] || Math.floor(Math.random() * 304);
   const curr = req.query['curr'] || Math.floor(Math.random() * 304);
 
   res.set('Content-Type', 'application/json');
 
-  Promise.all([
-    fsp.readFile('data/gtd.json', 'utf8'),
-    fsp.readFile('data/jira.json', 'utf8'),
-    fsp.readFile('shortcuts-win.json', 'utf8'),
-    fsp.readFile('shortcuts-mac.json', 'utf8')
-  ]).then(resolved => {
-        const winTips = JSON.parse(resolved[2]);
-        const macTips = JSON.parse(resolved[3]);
-        res.send(`{
-          "gtd": ${resolved[0]},
-          "jira": ${resolved[1]},
-          "prev": "${getTip(winTips, macTips, prev)}",
-          "curr": "${getTip(winTips, macTips, curr)}"
-        }`);
-      }
-  );
+  const resolved = await Promise.all([
+    fsp.readFile(SHORTCUTS_WIN, 'utf8'),
+    fsp.readFile(SHORTCUTS_MAC, 'utf8'),
+    fsp.readFile(DATA_GTD, 'utf8'),
+    fsp.readFile(DATA_JIRA, 'utf8'),
+    fsp.readFile(DATA_GUERRILLA, 'utf8')
+  ]);
+  const winTips = JSON.parse(resolved[0]);
+  const macTips = JSON.parse(resolved[1]);
+  const tips = winTips.concat(macTips);
+  await res.json({
+    prev: tips[prev],
+    curr: tips[curr],
+    gtd: JSON.parse(resolved[2]).filter(item => item.active),
+    jira: JSON.parse(resolved[3]).slice(0, 4),
+    guerrilla: JSON.parse(resolved[4]).filter(item => item.active)
+  });
 });
 
 // shortcuts page
 
 server.get('/shortcuts/:platform', (req, res) => {
   res.set('Content-Type', 'application/json');
-  res.sendFile(path.join(__dirname, `shortcuts-${req.params.platform}.json`));
+  res.sendFile(path.join(__dirname, 'assets', `shortcuts-${req.params.platform}.json`));
 });
 
 // endregion
 
 server.listen(PORT);
-logger.info(`Homepage ready at http://localhost:${PORT}`);
+console.log(`Homepage ready at http://localhost:${PORT}`);

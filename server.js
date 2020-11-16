f2e427c00dc77620c0c737a098e3ff47e2ec80f2
Chad Luo <chadl@booktopia.com.au>
2020-11-16 14:31:30 +1100

    formatter changes; fix dark mode not remembered

--- server.js
+++ server.js
@@ -21,40 +21,40 @@ const expressWinston = require("express-winston");
 const fs = require("fs");
 const fsp = fs.promises;
 const WINSTON_CONFIG = {
-    transports: [new winston.transports.Console()],
-    format: winston.format.combine(
-        winston.format.timestamp(),
-        winston.format.printf(i => `${i.timestamp} ${i.level} | ${i.message}`)
-    ),
-    meta: false,
-    expressFormat: true
+  transports: [new winston.transports.Console()],
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.printf(i => `${i.timestamp} ${i.level} | ${i.message}`)
+  ),
+  meta: false,
+  expressFormat: true
 };
 const logger = winston.createLogger(WINSTON_CONFIG);
 const path = require("path");
 
 db.defaults({
-    gtd: [],
-    jira: [],
-    guerrilla: [],
-    hackerNews: []
+  gtd: [],
+  jira: [],
+  guerrilla: [],
+  hackerNews: []
 }).write();
 
 const HACKER_NEWS_API = "https://hacker-news.firebaseio.com/v0/";
 const HACKER_NEWS_ITEM = `https://news.ycombinator.com/item?id=`;
 
 async function refreshHackerNews(limit) {
-    const topStoryIds = await axios.get(
-        `${HACKER_NEWS_API}/topstories.json?limitToFirst=${limit || 10}&orderBy="$priority"`
-    );
-    const topStories = await Promise.all(topStoryIds.data.map(id => axios.get(`${HACKER_NEWS_API}/item/${id}.json`)));
-    db.update("hackerNews", () =>
-        topStories.map(item => ({ title: item.data.title, url: item.data.url || `${HACKER_NEWS_ITEM}${item.data.id}` }))
-    ).write();
-    logger.info("Updated HN cache");
+  const topStoryIds = await axios.get(
+    `${HACKER_NEWS_API}/topstories.json?limitToFirst=${limit || 10}&orderBy="$priority"`
+  );
+  const topStories = await Promise.all(topStoryIds.data.map(id => axios.get(`${HACKER_NEWS_API}/item/${id}.json`)));
+  db.update("hackerNews", () =>
+    topStories.map(item => ({ title: item.data.title, url: item.data.url || `${HACKER_NEWS_ITEM}${item.data.id}` }))
+  ).write();
+  logger.info("Updated HN cache");
 }
 
 refreshHackerNews().then(); // init
-setInterval(refreshHackerNews, 60_000);
+setInterval(refreshHackerNews, 60000);
 
 const server = express();
 
@@ -67,98 +67,98 @@ server.use(express.json());
 server.get("/", (_req, res) => res.sendFile(path.join(__dirname, "homepage.html")));
 
 server.all("/gtd", async (req, res) => {
-    const payload = req.body;
-    switch (req.method) {
-        case "GET":
-            res.send(db.get("gtd").value());
-            break;
-        case "POST": // create
-            db.get("gtd")
-                .push(payload)
-                .write();
-            res.status(201).send();
-            break;
-        case "PUT": // update
-            payload.forEach(p => {
-                db.get("gtd")
-                    .find({ id: p.id })
-                    .assign({ todo: p.todo, active: p.active })
-                    .write();
-            });
-            res.send(db.get("gtd").filter({ active: true }));
-            break;
-        default:
-            res.status(405).send();
-            break;
-    }
+  const payload = req.body;
+  switch (req.method) {
+    case "GET":
+      res.send(db.get("gtd").value());
+      break;
+    case "POST": // create
+      db.get("gtd")
+        .push(payload)
+        .write();
+      res.status(201).send();
+      break;
+    case "PUT": // update
+      payload.forEach(p => {
+        db.get("gtd")
+          .find({ id: p.id })
+          .assign({ todo: p.todo, active: p.active })
+          .write();
+      });
+      res.send(db.get("gtd").filter({ active: true }));
+      break;
+    default:
+      res.status(405).send();
+      break;
+  }
 });
 
 // JIRA
 
 server.put("/jira", async (req, res) => {
-    const payload = req.body;
-    logger.info(`storing jira: ${payload.length} items`);
-    db.update("jira", () => payload).write();
-    res.send("ok");
+  const payload = req.body;
+  logger.info(`storing jira: ${payload.length} items`);
+  db.update("jira", () => payload).write();
+  res.send("ok");
 });
 
 // dynamic links ('guerrilla')
 
 server.put("/guerrilla", async (req, res) => {
-    const payload = req.body;
-    logger.info(`storing guerrilla: ${payload.length} items`);
-    db.update("guerrilla", () => payload).write();
-    res.send("ok");
+  const payload = req.body;
+  logger.info(`storing guerrilla: ${payload.length} items`);
+  db.update("guerrilla", () => payload).write();
+  res.send("ok");
 });
 
 // yibasuo
 
 server.get("/all", async (req, res) => {
-    const prev = req.query["prev"] || Math.floor(Math.random() * 304);
-    const curr = req.query["curr"] || Math.floor(Math.random() * 304);
+  const prev = req.query["prev"] || Math.floor(Math.random() * 304);
+  const curr = req.query["curr"] || Math.floor(Math.random() * 304);
 
-    res.set("Content-Type", "application/json");
+  res.set("Content-Type", "application/json");
 
-    const resolved = await Promise.all([
-        safeRead(SHORTCUTS_WIN),
-        safeRead(SHORTCUTS_MAC),
-        db
-            .get("gtd")
-            .filter({ active: true })
-            .value(),
-        db
-            .get("jira")
-            .take(5)
-            .value(),
-        db.get("guerrilla").value()
-    ]);
-    const winTips = JSON.parse(resolved[0]);
-    const macTips = JSON.parse(resolved[1]);
-    const tips = winTips.concat(macTips);
-    await res.json({
-        prev: tips[prev],
-        curr: tips[curr],
-        gtd: resolved[2],
-        jira: resolved[3],
-        guerrilla: resolved[4]
-    });
+  const resolved = await Promise.all([
+    safeRead(SHORTCUTS_WIN),
+    safeRead(SHORTCUTS_MAC),
+    db
+      .get("gtd")
+      .filter({ active: true })
+      .value(),
+    db
+      .get("jira")
+      .take(5)
+      .value(),
+    db.get("guerrilla").value()
+  ]);
+  const winTips = JSON.parse(resolved[0]);
+  const macTips = JSON.parse(resolved[1]);
+  const tips = winTips.concat(macTips);
+  await res.json({
+    prev: tips[prev],
+    curr: tips[curr],
+    gtd: resolved[2],
+    jira: resolved[3],
+    guerrilla: resolved[4]
+  });
 });
 
 async function safeRead(filePath) {
-    return fsp.readFile(filePath, "utf8");
+  return fsp.readFile(filePath, "utf8");
 }
 
 // shortcuts page
 
 server.get("/shortcuts/:platform", (req, res) => {
-    res.set("Content-Type", "application/json");
-    res.sendFile(path.join(__dirname, "assets", `shortcuts-${req.params.platform}.json`));
+  res.set("Content-Type", "application/json");
+  res.sendFile(path.join(__dirname, "assets", `shortcuts-${req.params.platform}.json`));
 });
 
 // hacker news
 
 server.get("/hn", (req, res) => {
-    res.json(db.get("hackerNews").value());
+  res.json(db.get("hackerNews").value());
 });
 
 // endregion

cfa6e75800ee3958b299b3a9cb8edd288e4437c6
chadluo <i@yuki.rocks>
2021-01-15 13:32:34 +1100

    update dependencies

--- wipe-dependencies.js
+++ wipe-dependencies.js
@@ -1,6 +1,9 @@
 /* https://medium.com/@jh3y/how-to-update-all-npm-packages-in-your-project-at-once-17a8981860ea */
 
-import { readFileSync, writeFileSync } from "fs";
+const fs = require("fs");
+const readFileSync = fs.readFileSync;
+const writeFileSync = fs.writeFileSync;
+
 const wipeDependencies = () => {
   const file = readFileSync("package.json");
   const content = JSON.parse(file);
